aa57276dfbb9b36258fb956f9edfae35782fabd6
AyeshaSiddiqua68 <akhiazad6868@gmail.com>
2021-10-30 01:34:16 +0600

    got api

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const cors=require('cors');
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ksgka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // console.log(uri);
@@ -13,7 +18,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log("database connected successfully");
+        // console.log("database connected successfully");
+        const database = client.db('food_delivery');
+        const foodCollection = database.collection('foods');
+
+        //GET foods API
+        app.get('/foods',async(req,res)=>{
+            const cursor=foodCollection.find({});
+           const foods=await cursor.toArray();
+           res.send(foods);
+        })
+        
     }
     finally {
         // await client.close()
