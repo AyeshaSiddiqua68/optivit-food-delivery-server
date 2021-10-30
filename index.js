9bf9ec8ed7cdc0193d860c31fff2320e825ee704
AyeshaSiddiqua68 <akhiazad6868@gmail.com>
2021-10-31 01:28:15 +0600

    data add and delete done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
-const cors=require('cors');
+const cors = require('cors');
+ const  ObjectID = require('mongodb').ObjectId;
 require('dotenv').config()
 
 const app = express()
@@ -21,14 +22,51 @@ async function run() {
         // console.log("database connected successfully");
         const database = client.db('food_delivery');
         const foodCollection = database.collection('foods');
+        const customerCollection = client.db("food_delivery").collection("customer");
+        const orderCollection = client.db("food_delivery").collection("orders");
 
         //GET foods API
-        app.get('/foods',async(req,res)=>{
-            const cursor=foodCollection.find({});
-           const foods=await cursor.toArray();
-           res.send(foods);
+        app.get('/foods', async (req, res) => {
+            const cursor = foodCollection.find({});
+            const foods = await cursor.toArray();
+            res.send(foods);
         })
-        
+
+        //place orders 
+        app.post('/placeOrder', async (req, res) => {
+            console.log(req.body);
+            const result = await orderCollection.insertOne(req.body);
+            console.log(result);
+        });
+
+        //Add customer
+        app.post("/addCustomer", async (req, res) => {
+            console.log(req.body);
+            const result = await customerCollection.insertOne(req.body);
+            res.send(result);
+        });
+
+        //Get all customers
+        app.get("/allCustomers", async (req, res) => {
+            const result = await customerCollection.find({}).toArray();
+            res.send(result);
+            console.log(object);
+        })
+
+        //Get all orders
+        app.get("/allFoods", async (req, res) => {
+            const result = await orderCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        //Delete order
+        app.delete("/deleteFood/:id", async (req, res) => {
+            console.log(req.params.id);
+            const result = await orderCollection.deleteOne({
+                _id: new ObjectID(req.params.id)
+            });
+            res.send(result);
+        });
     }
     finally {
         // await client.close()
